5fe15d251d39462d5e2b05ef36b15fe66f621a7e
Petróczi Zoltán <lgnd@tutanota.com>
2021-09-25 11:51:25 +0200

    Update

--- custom_components/freebsd_battery/__init__.py
+++ custom_components/freebsd_battery/__init__.py
@@ -17,16 +17,14 @@ BATTERY_STATUS_COMMAND = "sysctl hw.acpi.battery.state | cut -d' ' -f2"
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Linux Battery sensor."""
-    name = config.get(CONF_NAME)
-    add_entities([LinuxBatterySensor(name)], True)
+    add_entities([LinuxBatterySensor()], True)
 
 
 class LinuxBatterySensor(SensorEntity):
     """Representation of a Linux Battery sensor."""
 
-    def __init__(self, name):
+    def __init__(self):
         """Initialize the battery sensor."""
-        self._name = name
         self._battery_stat = {
             ATTR_CAPACITY_LEVEL: None,
             ATTR_STATUS: None
@@ -35,7 +33,7 @@ class LinuxBatterySensor(SensorEntity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
+        return "FreeBSD Battery"
 
     @property
     def device_class(self):
@@ -43,26 +41,23 @@ class LinuxBatterySensor(SensorEntity):
         return DEVICE_CLASS_BATTERY
 
     @property
-    def native_value(self):
+    def current_capacity(self):
         """Return the state of the sensor."""
-        return self._battery_stat
+        return self._battery_stat[ATTR_CAPACITY_LEVEL]
 
     @property
-    def native_unit_of_measurement(self):
+    def is_charging(self):
+        """Return the state of the sensor."""
+        return self._battery_stat[ATTR_STATUS] == 1
+
+    @property
+    def unit_of_measurement(self):
         """Return the unit the value is expressed in."""
         return PERCENTAGE
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes of the sensor."""
-        return {
-            ATTR_CAPACITY_LEVEL: self._battery_stat[ATTR_CAPACITY_LEVEL],
-            ATTR_STATUS: self._battery_stat[ATTR_STATUS],
-        }
 
     def update(self):
         """Get the latest data and updates the states."""
         battery_life = int(os.system(BATTERY_LIFE_COMMAND))
-        battery_status = os.system(BATTERY_STATUS_COMMAND) == "1"
+        battery_status = int(os.system(BATTERY_STATUS_COMMAND))
         self._battery_stat[ATTR_CAPACITY_LEVEL] = battery_life
         self._battery_stat[ATTR_STATUS] = battery_status
